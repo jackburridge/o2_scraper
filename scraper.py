55958ea82fb0ccbb4a1d9ffe88198b8d1a1ebf23
jburridge <jack.burridge@mail.com>
2016-10-21 15:51:12 +0100

    Initial trace but using sleeps

--- scraper.py
+++ scraper.py
@@ -1,10 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
-driver = webdriver.Chrome("/usr/lib/chromium/chromedriver")
-driver.get("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk")
+class Scraper:
+    def __init__(self):
+        self.driver = webdriver.Chrome("/usr/lib/chromium/chromedriver")
+        self.driver.get("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk")
+        self.driver.implicitly_wait(2)
 
-countryName = driver.find_element_by_id("countryName")
-countryName.clear()
-countryName.send_keys("Canada")
-countryName.send_keys(Keys.RETURN)
+
+    def searchCountry(self,country):
+        countryName = self.driver.find_element_by_id("countryName")
+        countryName.clear()
+        countryName.send_keys(country)
+        countryName.send_keys(Keys.RETURN)
+        time.sleep(1)
+
+
+    def getLandlinePrice(self):
+        paymonthly = self.driver.find_element_by_id("paymonthly")
+        paymonthly.click()
+
+        landlinePrice = self.driver.find_element_by_xpath("//*[@id=\"standardRatesTable\"]/tbody/tr[1]/td[2]")
+        time.sleep(1)
+        return landlinePrice.text
+
+if __name__ == "__main__":
+    s = Scraper()
+    for country in ["Canada","Germany", "Iceland", "Pakistan", "Singapore", "South Africa"]:
+        s.searchCountry(country)
+        price = s.getLandlinePrice()
+        print "Country:", country
+        print "Price:" ,price
